bd74a7528e17003901ca1cc57581d5e7f6e59910
Jibru-ski <barakajibran2004@gmail.com>
2025-08-28 18:16:31 +0300

    Transactions Board build

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { HStack, Icon, Stat, Text } from '@chakra-ui/react'
+import { HStack, VStack, Icon, Stat, Text, Button, EmptyState, ButtonGroup } from '@chakra-ui/react'
+import { HiColorSwatch } from "react-icons/hi"
 
 const Dashboard = () => {
   return (
     <>
       <h1 className='welcome-message'>Hello JDoe01👋</h1>
       <Text fontSize={14} mb={2}>Payment Updates</Text>
-      <HStack gap={5}>
+      <HStack gap={5} mb={10}>
         <div className="stat-card">
           <Stat.Root>
             <div className="flex justify-between">
@@ -52,6 +53,31 @@ const Dashboard = () => {
           </Stat.Root>
         </div>
       </HStack>
+      <div className="w-[40em]">
+        <div className="flex justify-between items-center">
+          <Text fontSize={14} mb={2}>Latest Transactions</Text>
+          <Button variant='subtle' size='xs' href='#'>View All</Button>
+        </div>
+        <div className="transaction-board">
+          <EmptyState.Root>
+            <EmptyState.Content>
+              <EmptyState.Indicator>
+                <HiColorSwatch />
+              </EmptyState.Indicator>
+              <VStack textAlign="center">
+                <EmptyState.Title>Start making Transactions</EmptyState.Title>
+                <EmptyState.Description>
+                  Your statement will be ready once you complete a transaction
+                </EmptyState.Description>
+              </VStack>
+              <ButtonGroup>
+                <Button>Add Transaction</Button>
+                <Button variant="outline">Learn How</Button>
+              </ButtonGroup>
+            </EmptyState.Content>
+          </EmptyState.Root>
+        </div>
+      </div>
     </>
   )
 }
