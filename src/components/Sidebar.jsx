395b6f35e4284bdea53239d440f23cc63f85e460
Jibru-ski <barakajibran2004@gmail.com>
2025-08-28 15:16:51 +0300

    Sidebar 2nd Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,32 +4,34 @@ import { Avatar, VStack, Link, Stack, Text, Button } from '@chakra-ui/react'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-    <VStack p={3} height="100%">
-        <div>
-            <Text fontSize="xl" fontWeight="bold" mb="8">Expense Planner</Text>
-            <div className="">
-                <Button variant='plain' href="#" size='lg'>
-                    <Avatar.Root>
+    <VStack p={4} height="100%" gap={5}>
+        <div className='sidebar-header'>
+            <Text fontSize="md">
+                Expense Planner
+            </Text>
+        </div>
+        <div className="sidebar-header2">
+            <Button variant='plain' href="#" size="xs" ml={1}>
+                    <Avatar.Root colorPalette="teal">
                         <Avatar.Image 
                             src="https://bit.ly/broken-link" 
                             alt="User Avatar" 
                             boxSize="40px" 
                             borderRadius="full" 
                         />
-                        <Avatar.Fallback name="User"/>
+                        <Avatar.Fallback name="John Doe"/>
                     </Avatar.Root>
-                    <Text fontSize="sm">Username</Text>
+                    <Text ml={2} fontSize="sm">John Doe</Text>
                 </Button>
-            </div>
         </div>
         <nav>
             <Stack spacing={4} w={180}>
-                <Button size='sm' variant='outline' href='#'><i class='bx bxs-dashboard'></i>Dashboard</Button>
-                <Button size='sm' variant='outline' href='#'><i class='bx bx-money-withdraw' ></i>Transactions</Button>
-                <Button size='sm' variant='outline' href='#'><i class='bx bxs-bank' ></i>Accounts</Button>
-                <Button size='sm' variant='outline' href='#'><i class='bx bxs-calculator' ></i>Calculators</Button>
-                <Button size='sm' variant='outline' href='#'><i class='bx bx-line-chart' ></i>Analytics</Button>
-                <Button size='sm' variant='outline' href='#'><i class='bx bx-cog' ></i>Settings</Button>
+                <Button justifyContent="start" pl={8} size='sm' variant='outline' href='#'><i class='bx bxs-dashboard'></i>Dashboard</Button>
+                <Button justifyContent="start" pl={8} size='sm' variant='outline' href='#'><i class='bx bx-money-withdraw' ></i>Transactions</Button>
+                <Button justifyContent="start" pl={8} size='sm' variant='outline' href='#'><i class='bx bxs-bank' ></i>Accounts</Button>
+                <Button justifyContent="start" pl={8} size='sm' variant='outline' href='#'><i class='bx bxs-calculator' ></i>Calculators</Button>
+                <Button justifyContent="start" pl={8} size='sm' variant='outline' href='#'><i class='bx bx-line-chart' ></i>Analytics</Button>
+                <Button justifyContent="start" pl={8} size='sm' variant='outline' href='#'><i class='bx bx-cog' ></i>Settings</Button>
             </Stack>
         </nav>
         
