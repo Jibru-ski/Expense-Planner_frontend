fe23c820dbc0372acfe7dc38b121811348fb94e9
Jibru-ski <barakajibran2004@gmail.com>
2025-09-01 09:21:28 +0300

    Acc Page and Data Rendering

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,43 +1,55 @@
 import React from 'react'
 import { Text, Card, Tag, Stat, FormatNumber, Button } from '@chakra-ui/react'
+import { getAccounts } from '../api/accountservice'
 
 const Accounts = () => {
-    
+    const [accounts, setAccounts] = React.useState([]);
+    const [active, SetActive] = React.useState();
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const accountData = await getAccounts();
+            setAccounts(accountData)
+        }
+
+        fetchData();
+    })
 
   return (
     <>
     <Text fontSize={20} fontWeight={500} mb={5}><i class='bx bxs-bank' ></i>Accounts</Text>
-    <div className="">
-        <Card.Root maxW="sm">
-            <Card.Header>
-                <Card.Description className='flex justify-between'>
-                    Default
-                    <Button size='xs' variant='plain'><i class='bx bx-edit'></i></Button>
-                </Card.Description>
-                <div className="flex">
-                    <Card.Title>Primary Account</Card.Title>
-                    <Tag.Root rounded={15} ml={4} pl={3} pr={3} size="sm" colorPalette="green">
-                        <Tag.Label>Active</Tag.Label>
-                    </Tag.Root>
+    {accounts.map((acc) => (
+        <div className="">
+            <Card.Root maxW="sm">
+                <Card.Header>
+                    <Card.Description className='flex justify-between'>
+                       {acc.name == 'Primary Account' ? 'Default' : 'Secondary'}
+                        <Button size='xs' variant='plain'><i class='bx bx-edit'></i></Button>
+                    </Card.Description>
+                    <div className="flex">
+                        <Card.Title>{acc.name}</Card.Title>
+                        <Tag.Root rounded={15} ml={4} pl={3} pr={3} size="sm" colorPalette={acc.isActive ? 'green' : 'red'}>
+                            <Tag.Label>{acc.isActive ? 'Active' : 'Inactive'}</Tag.Label>
+                        </Tag.Root>
+                    </div>
+                </Card.Header>
+                <div className='border-b-2'>
+                    <Card.Body>
+                        Type: {acc.type}<br/>
+                        Created On: {acc.createdOn}
+                    </Card.Body>              
                 </div>
-            </Card.Header>
-            <div className='border-b-2'>
-                <Card.Body>
-                    Type: Bank<br/>
-                    Created On: 12/03/25
-                </Card.Body>              
-            </div>
-            <Card.Footer>
-                <Stat.Root>
-                    <Stat.Label>Balance</Stat.Label>
-                    <Stat.ValueText>
-                        <FormatNumber value={935.4} style="currency" currency="USD" />
-                    </Stat.ValueText>
-                </Stat.Root>
-                <Button size="xs">View Transactions</Button>
-            </Card.Footer>
-        </Card.Root>
-    </div>
+                <Card.Footer>
+                    <Stat.Root>
+                        <Stat.Label>Balance</Stat.Label>
+                        <Stat.ValueText>
+                            <FormatNumber value={acc.balance} style="currency" currency="USD" />
+                        </Stat.ValueText>
+                    </Stat.Root>
+                    <Button size="xs">View Transactions</Button>
+                </Card.Footer>
+            </Card.Root>
+        </div>
+    ))}
     </>
   )
 }
